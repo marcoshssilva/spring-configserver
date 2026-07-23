71f93f06b59c02ca7a6bdfcc5442257b3e462560
Marcos Henrique de Santana <76234459+marcoshssilva@users.noreply.github.com>
2026-07-23 02:40:42 -0300

    refactor: Update package structure and project metadata
    - Moved package from `br.com.marcoshssilva` to `com.github.marcoshssilva`.
    - Updated project version to `0.1.0-RC-SNAPSHOT` in `pom.xml`.
    - Added `@ActiveProfiles("test")` annotation for test configuration.

--- src/main/java/com/github/marcoshssilva/springconfigserver/SpringConfigserverApplication.java
+++ src/main/java/com/github/marcoshssilva/springconfigserver/SpringConfigserverApplication.java
@@ -1,4 +1,4 @@
-package br.com.marcoshssilva.spring_configserver;
+package com.github.marcoshssilva.springconfigserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/test/java/com/github/marcoshssilva/springconfigserver/SpringConfigserverApplicationTests.java
+++ src/test/java/com/github/marcoshssilva/springconfigserver/SpringConfigserverApplicationTests.java
@@ -1,8 +1,10 @@
-package br.com.marcoshssilva.spring_configserver;
+package com.github.marcoshssilva.springconfigserver;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
+@ActiveProfiles({ "test" })
 @SpringBootTest
 class SpringConfigserverApplicationTests {
 
